16fc5a9567388ac004e962c63d8f7eea071948e7
nicolasigor <nicolasigor.tapia@gmail.com>
2018-06-27 00:11:19 -0400

    normalization of features

--- feedforward/estimator_fc.py
+++ feedforward/estimator_fc.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 def main():
     # Load dataset
-    train_path = "pickle_data/n2fft_dataframe_lite_train.pkl"
-    test_path = "pickle_data/n2fft_dataframe_lite_test.pkl"
+    train_path = "pickle_data/n2fft_dataframe_full_train.pkl"
+    test_path = "pickle_data/n2fft_dataframe_full_test.pkl"
 
     # Training set
     train_df = pd.read_pickle(train_path)
@@ -25,23 +25,30 @@ def main():
     test_labels = test_df['MARK'].values.astype(int)
     del test_df
 
+    # Normalize data
+    mean_train = np.mean(train_features, axis=0)
+    std_train = np.std(train_features, axis=0)
+    train_features = (train_features - mean_train)/std_train
+    train_features = (test_features - mean_train)/std_train
+
     # Specify that all features have real-value data
     feature_columns = [tf.feature_column.numeric_column("x", shape=train_features.shape[1])]
     weight_column = tf.feature_column.numeric_column("weight")
 
     # Build 2 layer DNN with 128, 64, 1 units respectively
     classifier = tf.estimator.DNNClassifier(feature_columns=feature_columns,
-                                            hidden_units=[128],
+                                            hidden_units=[128, 64],
                                             n_classes=2,
                                             model_dir="model_data/spindle_estimator",
                                             optimizer=tf.train.AdamOptimizer(learning_rate=0.01),
-                                            weight_column=weight_column)
+                                            weight_column=weight_column,
+                                            dropout=0.3)
 
     # Define the training inputs
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
         x={"x": train_features, "weight": train_weight},
         y=train_labels,
-        num_epochs=10,
+        num_epochs=50,
         shuffle=True,
         batch_size=32
     )
