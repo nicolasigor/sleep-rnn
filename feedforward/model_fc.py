c417f524d7aeec2829a6bc99dc9a3eb2d5f71949
nicolasigor <nicolasigor.tapia@gmail.com>
2018-06-26 23:48:52 -0400

    estimator

--- feedforward/model_fc.py
+++ feedforward/model_fc.py
@@ -1,2 +1,40 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
+import pickle
+
+class SpindleFC(object):
+    def __init__(self):
+
+
+    def train(self, train_path):
+        self.train_path = train_path
+        train_df = pd.read_pickle(train_path)
+        features = np.stack(train_df['FFT_DATA'].values)
+        labels = train_df['MARK'].values.astype(int)
+        del train_df
+        
+
+    def predict(self, test_path):
+        pass
+
+    def load_checkpoint(self, checkpoint_path):
+        pass
+
+    def _data_init(self):
+        pass
+
+    def _model_init(self):
+        pass
+
+    def _loss_init(self):
+        pass
+
+    def _optimizer_init(self):
+        pass
+
+
+if __name__ == "__main__":
+    train_path = "pickle_data/n2fft_dataframe_lite_train.pkl"
+    model = SpindleFC()
+    model.train(train_path)
