9ca7019ed0e7292e2b2b9ecca658b995dbba5e3c
nicolasigor <nicolasigor.tapia@gmail.com>
2019-06-19 15:07:14 -0400

    added working multipy bash script. Grid for v11 and v12 on SS and KC using norm_global normalization

--- scripts/multi_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-screen -S screen_script1 -d -m -- bash -c ' source activate tf ; python script1.py  ; exec $SHELL' ; 
-screen -S screen_script2 -d -m -- bash -c ' source activate tf ; python script2.py  ; exec $SHELL' ;
-screen -S screen_script3 -d -m -- bash -c ' source activate tf ; python script3.py  ; exec $SHELL' ; 
-screen -S screen_script4 -d -m -- bash -c ' source activate tf ; python script4.py  ; exec $SHELL' 
