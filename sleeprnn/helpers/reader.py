d3038bee7969f4075f58556f70edef445bedd862
nicolasigor <nicolasigor.tapia@gmail.com>
2021-02-11 18:57:19 -0300

    cap-a7 dataset implemented and checked

--- sleeprnn/helpers/reader.py
+++ sleeprnn/helpers/reader.py
@@ -9,6 +9,7 @@ from sleeprnn.data.inta_ss import IntaSS
 from sleeprnn.data.mass_kc import MassKC
 from sleeprnn.data.mass_ss import MassSS
 from sleeprnn.data.isruc_ss import IsrucSS
+from sleeprnn.data.cap_ss import CapSS
 
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '../..')
 RESULTS_PATH = os.path.join(PROJECT_ROOT, 'results')
@@ -153,7 +154,8 @@ def load_dataset(dataset_name, load_checkpoint=True, params=None, verbose=True, 
             constants.MASS_KC_NAME,
             constants.MASS_SS_NAME,
             constants.INTA_SS_NAME,
-            constants.ISRUC_SS_NAME
+            constants.ISRUC_SS_NAME,
+            constants.CAP_SS_NAME
         ])
     if dataset_name == constants.MASS_SS_NAME:
         dataset = MassSS(
@@ -163,6 +165,9 @@ def load_dataset(dataset_name, load_checkpoint=True, params=None, verbose=True, 
             load_checkpoint=load_checkpoint, params=params, verbose=verbose)
     elif dataset_name == constants.ISRUC_SS_NAME:
         dataset = IsrucSS(
+            load_checkpoint=load_checkpoint, params=params, verbose=verbose, **kwargs)
+    elif dataset_name == constants.CAP_SS_NAME:
+        dataset = CapSS(
             load_checkpoint=load_checkpoint, params=params, verbose=verbose, **kwargs)
     else:
         dataset = IntaSS(
