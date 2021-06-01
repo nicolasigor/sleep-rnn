789b29a2acfb9f6facff43729c1ba140ec3ecbd7
nicolasigor <nicolasigor.tapia@gmail.com>
2021-06-01 18:23:04 -0400

    optimal thr of thesis indata experiment

--- baselines_scripts/train_a7.py
+++ baselines_scripts/train_a7.py
@@ -49,37 +49,53 @@ if __name__ == '__main__':
 
     save_fitting = True
 
-    # Dataset training settings
     fs = 200
-    dataset_name = constants.MASS_SS_NAME
-    which_expert = 1
-    strategy = 'fixed'  # {'fixed' or '5cv'}
-    n_seeds = 11
-    average_mode = constants.MACRO_AVERAGE
     iou_threshold_report = 0.2
-
+    # Dataset training settings
+    configs = [
+        dict(
+            dataset_name=constants.MASS_SS_NAME, which_expert=1,
+            strategy='fixed', n_seeds=11, average_mode=constants.MACRO_AVERAGE),
+        dict(
+            dataset_name=constants.MASS_SS_NAME, which_expert=2,
+            strategy='fixed', n_seeds=11, average_mode=constants.MACRO_AVERAGE),
+        dict(
+            dataset_name=constants.MASS_SS_NAME, which_expert=1,
+            strategy='5cv', n_seeds=3, average_mode=constants.MACRO_AVERAGE),
+        dict(
+            dataset_name=constants.MASS_SS_NAME, which_expert=2,
+            strategy='5cv', n_seeds=3, average_mode=constants.MACRO_AVERAGE),
+    ]
     # ------------------------------------------------------
+    for config in configs:
+        dataset_name = config['dataset_name']
+        which_expert = config['which_expert']
+        strategy = config['strategy']  # {'fixed' or '5cv'}
+        n_seeds = config['n_seeds']
+        average_mode = config['average_mode']
+        # Fit ID
+        fit_id = "%s_e%d_%s" % (dataset_name, which_expert, strategy)
+        print("\nEvaluation of %s (%s)" % (fit_id, average_mode))
+        # Load dataset
+        dataset = reader.load_dataset(dataset_name, verbose=True, params={pkeys.FS: fs})
+        # Predictions
+        pred_dir = os.path.join(BASELINES_DATA_PATH, BASELINE_FOLDER, 'output_thesis_%s' % dataset_name.split("_")[0])
+        print('Loading predictions from %s' % pred_dir, flush=True)
+        settings = butils.get_settings(pred_dir, extract_setting)
+        pred_dict = butils.get_prediction_dict(dataset, pred_dir, settings, get_raw_marks)
+        train_ids_list, _, test_ids_list = butils.get_partitions(dataset, strategy, n_seeds)
+        fitted_setting_dict = butils.train_grid(dataset, which_expert, train_ids_list, pred_dict, average_mode)
 
-    # Load dataset
-    dataset = reader.load_dataset(dataset_name, verbose=True, params={pkeys.FS: fs})
-    # Predictions
-    pred_dir = os.path.join(BASELINES_DATA_PATH, BASELINE_FOLDER, 'output_thesis_%s' % dataset_name.split("_")[0])
-    print('Loading predictions from %s' % pred_dir, flush=True)
-    settings = butils.get_settings(pred_dir, extract_setting)
-    pred_dict = butils.get_prediction_dict(dataset, pred_dir, settings, get_raw_marks)
-    train_ids_list, _, test_ids_list = butils.get_partitions(dataset, strategy, n_seeds)
-    fitted_setting_dict = butils.train_grid(dataset, which_expert, train_ids_list, pred_dict, average_mode)
-    fit_id = "%s_e%d" % (dataset_name, which_expert)
+        if save_fitting:
+            fitting_save_dir = os.path.join(BASELINES_SAVE_PATH, BASELINE_FOLDER)
+            os.makedirs(fitting_save_dir, exist_ok=True)
+            fname = os.path.join(fitting_save_dir, 'fitted_%s.json' % fit_id)
+            with open(fname, 'w') as outfile:
+                json.dump(fitted_setting_dict, outfile)
 
-    if save_fitting:
-        fitting_save_dir = os.path.join(BASELINES_SAVE_PATH, BASELINE_FOLDER)
-        os.makedirs(fitting_save_dir, exist_ok=True)
-        fname = os.path.join(fitting_save_dir, 'fitted_%s.json' % fit_id)
-        with open(fname, 'w') as outfile:
-            json.dump(fitted_setting_dict, outfile)
-
-    results = butils.evaluate_by_fold(
-        dataset, which_expert, test_ids_list, fitted_setting_dict, pred_dict, average_mode, iou_threshold_report)
-    print("\nTest performance (%s, iou >= %1.2f, from train to test) for %s" % (
-        average_mode, iou_threshold_report, fit_id))
-    butils.print_performance(results)
+        results = butils.evaluate_by_fold(
+            dataset, which_expert, test_ids_list, fitted_setting_dict, pred_dict, average_mode, iou_threshold_report)
+        print("\nTest performance (%s, iou >= %1.2f, from train to test) for %s" % (
+            average_mode, iou_threshold_report, fit_id))
+        butils.print_performance(results)
+        del dataset
